00af58d232fb5b3885c7f6f44bdd0c509f5a01a5
TakehiroTada <taketada.works@gmail.com>
2019-10-18 09:29:03 +0900

    :art: delete default styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style>
-html {
+/* html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
@@ -51,5 +51,5 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
-}
+} */
 </style>
